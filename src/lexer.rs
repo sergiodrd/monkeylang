8cd7398b7dbc728a41f9891c711ddc7cd7abec31
sergio <sergiod.roddj@gmail.com>
2023-06-10 21:24:01 -0400

    expanded the lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -16,7 +16,7 @@ pub enum Token {
     // Delimiters
     Comma,
     Semicolon,
-    
+
     LParen,
     RParen,
     LBrace,
@@ -47,21 +47,32 @@ impl Lexer {
     }
 
     pub fn next_token(&mut self) -> Result<Token> {
+        self.skip_whitespace();
+
         let token = match self.ch {
-            Some(t) => {
-                match t {
-                    '=' => Token::Assign,
-                    ';' => Token::Semicolon,
-                    '(' => Token::LParen,
-                    ')' => Token::RParen,
-                    ',' => Token::Comma,
-                    '+' => Token::Plus,
-                    '{' => Token::LBrace,
-                    '}' => Token::RBrace,
-                    _ => Token::Illegal
+            Some(t) => match t {
+                '=' => Token::Assign,
+                ';' => Token::Semicolon,
+                '(' => Token::LParen,
+                ')' => Token::RParen,
+                ',' => Token::Comma,
+                '+' => Token::Plus,
+                '{' => Token::LBrace,
+                '}' => Token::RBrace,
+                'a'..='z' | 'A'..='Z' | '_' => {
+                    let ident = self.read_ident();
+                    return Ok(match ident.as_str() {
+                        "fn" => Token::Function,
+                        "let" => Token::Let,
+                        _ => Token::Ident(ident)
+                    });
                 }
-            }
-            None => Token::EOF
+                '0'..='9' => {
+                    return Ok(Token::Int(self.read_int()));
+                }
+                _ => Token::Illegal,
+            },
+            None => Token::EOF,
         };
 
         self.read_char();
@@ -70,28 +81,52 @@ impl Lexer {
 }
 
 impl Lexer {
-    fn read_char(&mut self) -> Option<char> {
-        let c = self.input.chars().nth(self.read_position);
-
-        if let Some(ch) = c {
+    fn read_char(&mut self) {
+        if let Some(ch) = self.input.chars().nth(self.read_position) {
             self.position = self.read_position;
             self.read_position += 1;
             self.ch = Some(ch);
         } else {
             self.ch = None;
         }
+    }
 
-        c
+    fn skip_whitespace(&mut self) {
+        while self.ch.is_some() && self.ch.unwrap().is_ascii_whitespace() {
+            self.read_char();
+        }
+    }
+
+    fn read_ident(&mut self) -> String {
+        let mut out = String::new();
+        while self.ch.is_some() && Self::is_ident_char(self.ch.unwrap()) {
+            out.push(self.ch.unwrap());
+            self.read_char();
+        }
+        out
+    }
+
+    fn is_ident_char(ch: char) -> bool {
+        'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+    }
+
+    fn read_int(&mut self) -> i32 {
+        let mut out = String::new();
+        while self.ch.is_some() && self.ch.unwrap().is_ascii_digit() {
+            out.push(self.ch.unwrap());
+            self.read_char();
+        }
+        out.parse().unwrap()
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use anyhow::{Result, Ok};
+    use anyhow::{Ok, Result};
 
     #[test]
-    fn it_works() -> Result<()>{
+    fn it_works() -> Result<()> {
         let mut lexer = Lexer::new("=+(){},;".to_string());
 
         let tokens = vec![
@@ -110,7 +145,72 @@ mod tests {
             let next_token = lexer.next_token()?;
             println!("expected: {:?}, received {:?}", token, next_token);
             assert_eq!(token, next_token);
-        } 
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn it_works_better() -> Result<()> {
+        let source = r#"
+        let five = 5;
+        let ten = 10;
+
+        let add = fn(x, y) {
+            x + y;
+        };
+
+        let result = add(five, ten);
+        "#
+        .to_string();
+
+        let mut lexer = Lexer::new(source);
+
+        let tokens = vec![
+            Token::Let,
+            Token::Ident(String::from("five")),
+            Token::Assign,
+            Token::Int(5),
+            Token::Semicolon,
+            Token::Let,
+            Token::Ident(String::from("ten")),
+            Token::Assign,
+            Token::Int(10),
+            Token::Semicolon,
+            Token::Let,
+            Token::Ident(String::from("add")),
+            Token::Assign,
+            Token::Function,
+            Token::LParen,
+            Token::Ident(String::from("x")),
+            Token::Comma,
+            Token::Ident(String::from("y")),
+            Token::RParen,
+            Token::LBrace,
+            Token::Ident(String::from("x")),
+            Token::Plus,
+            Token::Ident(String::from("y")),
+            Token::Semicolon,
+            Token::RBrace,
+            Token::Semicolon,
+            Token::Let,
+            Token::Ident(String::from("result")),
+            Token::Assign,
+            Token::Ident(String::from("add")),
+            Token::LParen,
+            Token::Ident(String::from("five")),
+            Token::Comma,
+            Token::Ident(String::from("ten")),
+            Token::RParen,
+            Token::Semicolon,
+            Token::EOF,
+        ];
+
+        for token in tokens {
+            let next_token = lexer.next_token()?;
+            println!("expected: {:?}, received {:?}", token, next_token);
+            assert_eq!(token, next_token);
+        }
 
         Ok(())
     }
